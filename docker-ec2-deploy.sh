6c677d77a3412e5851e94115b63aeef2bc9d7767
aarrechea <adrianarrechea@yahoo.com.ar>
2024-04-13 17:40:23 -0300

    docker deploy

---
+++ docker-ec2-deploy.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+TARGET='main'
+
+cd ~/api || exit
+
+ACTION_COLOR='\033[1;90m'
+NO_COLOR='\033[0m'
+
+echo -e ${ACTION_COLOR} Checking if we are on the target branch
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [ "$BRANCH" != ${TARGET} ]
+then
+    exit 0
+fi
+
+#Checking if the repository is up to date
+git fetch
+HEAD_HASH=$(git rev-parse HEAD)
+UPSTREAM_HASH=$(git rev-parse ${TARGET}@{upstream})
+
+if ["$HEAD_HASH" == "UPSTREAM_HASH"]
+then
+    echo -e "${FINISHED}"The current bransh is up to date with origin/${TARGET}."${NO_COLOR}"
+    exit 0
+fi
+
+# if there are new changes, we pull these changes
+git pull origin main;
+
+# we can now build and start the containers
+docker compose up -d --build
+exit 0;
+
+
+
