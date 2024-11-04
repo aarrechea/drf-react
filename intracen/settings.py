56403575b15eab04a6c92dca413ea88b3fbd293d
aarrechea <adrianarrechea@yahoo.com.ar>
2024-11-04 10:38:48 -0300

    cors to first place in middleware

--- intracen/settings.py
+++ intracen/settings.py
@@ -64,9 +64,10 @@ INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
 
 # Middleware
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    #'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
