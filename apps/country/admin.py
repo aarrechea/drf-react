9bcf9c4d5a6aa4a2af66d44a97c01f14a914d85e
aarrechea <adrianarrechea@yahoo.com.ar>
2024-11-05 21:10:59 -0300

    added admin for other models

--- apps/country/admin.py
+++ apps/country/admin.py
@@ -1,3 +1,31 @@
+# Imports
 from django.contrib import admin
+from apps.country.models import Country, Region, Continent
 
-# Register your models here.
+
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 
+        'name',
+        'inhabitants',
+        'continent',
+        'region',
+    )
+    
+    
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 
+        'name',        
+    )
+    
+@admin.register(Continent)
+class ContinentAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 
+        'name',        
+    )
--- apps/industry/admin.py
+++ apps/industry/admin.py
@@ -1,3 +1,40 @@
+# Imports
 from django.contrib import admin
+from apps.industry.models import Industry, Sector, Supersector, Subsector
 
-# Register your models here.
+
+
+
+@admin.register(Industry)
+class IndustryAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 
+        'name',        
+    )
+    
+    
+@admin.register(Supersector)
+class SupersectorAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 
+        'name',        
+        'industry'
+    )
+
+
+@admin.register(Sector)
+class SectorAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 
+        'name',  
+        'supersector'      
+    )
+    
+    
+@admin.register(Subsector)
+class SubsectorAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 
+        'name',        
+        'sector'
+    )
